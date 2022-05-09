c29d3f877df83b977736b2a642b49afa6db2390f
SrPollo25 <daniel.ortega.alu@santamaria-artesano.org>
2022-05-09 09:56:45 +0200

    first commit

---
+++ BD1/src/baseDeDatos/Conexion.java
@@ -0,0 +1,72 @@
+package baseDeDatos;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Logger;
+import java.util.logging.Level;
+
+public class Conexion {
+
+    private Connection conexion;
+
+    /**
+     * Hace un testeo para confirmar de que la libreria de la base de datos
+     * existe
+     *
+     * @return
+     */
+    public int registrarDriver() {
+        int resultado;
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");//una vez creado este el 
+            //resto se añade en el solucionador de problemas
+            resultado = 0;
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(Conexion.class.getName())
+                    .log(Level.SEVERE, null, ex);
+            resultado = -1;
+        }
+        return resultado;
+    }
+
+    public Connection getConexion() {
+        return conexion;
+    }
+
+    /**
+     * Establece la conexion
+     *
+     * @param url
+     * @return retorna por si fallos
+     */
+    public int establecer(String url) {
+        int resultado;
+        resultado = registrarDriver();//Comprueba si esta el driver
+        if (resultado != -1) {//Si no está no conecta
+            resultado = conectar(url);//Conecta con la base
+        }
+
+        return resultado;
+    }
+
+    /**
+     * Hace conexion con el servidor
+     *
+     * @param url
+     * @return retorna un numero con el numero del fallo
+     */
+    public int conectar(String url) {
+        int resultado;
+        try {
+            //Se utiliza la contraseña y usuario de la base de datos
+            conexion = DriverManager.getConnection(url, "root", "root");
+        } catch (SQLException ex) {
+            Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        resultado = 0;
+        return resultado;
+    }
+
+}
---
+++ BD1/src/vista/VtnLogin.java
@@ -0,0 +1,134 @@
+
+package vista;
+
+import baseDeDatos.Conexion;
+import javax.swing.JOptionPane;
+
+
+public class VtnLogin extends javax.swing.JFrame {
+
+    Conexion bd;
+    public VtnLogin() {
+        int resultado;
+        initComponents();
+        bd=new Conexion();
+        /*
+        jdbc:mysql: + // + (ubicacion del server) + (puerto por donde pasa, 33 generalmente)
+         */
+        resultado=bd.establecer("jdbc:mysql://localhost:3306/prog_primera_base");
+        if(bd.registrarDriver()!=0){
+            JOptionPane.showConfirmDialog(this, "queso");
+        }
+        
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jLabel1 = new javax.swing.JLabel();
+        pswPass = new javax.swing.JPasswordField();
+        txtLogin = new javax.swing.JTextField();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jButton1 = new javax.swing.JButton();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jLabel1.setFont(new java.awt.Font("Consolas", 1, 36)); // NOI18N
+        jLabel1.setText("LOGIN");
+
+        jLabel2.setText("User");
+
+        jLabel3.setText("Pasword");
+
+        jButton1.setFont(new java.awt.Font("Dialog", 1, 24)); // NOI18N
+        jButton1.setText("Login");
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jButton1)
+                .addGap(30, 30, 30))
+            .addGroup(layout.createSequentialGroup()
+                .addGap(72, 72, 72)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabel2, javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(jLabel3, javax.swing.GroupLayout.Alignment.TRAILING))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(pswPass)
+                            .addComponent(txtLogin, javax.swing.GroupLayout.PREFERRED_SIZE, 109, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                    .addComponent(jLabel1))
+                .addContainerGap(162, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLabel1)
+                .addGap(57, 57, 57)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(txtLogin, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel2))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(pswPass, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel3))
+                .addGap(11, 11, 11)
+                .addComponent(jButton1)
+                .addContainerGap(71, Short.MAX_VALUE))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(VtnLogin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(VtnLogin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(VtnLogin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(VtnLogin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new VtnLogin().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JPasswordField pswPass;
+    private javax.swing.JTextField txtLogin;
+    // End of variables declaration//GEN-END:variables
+}
